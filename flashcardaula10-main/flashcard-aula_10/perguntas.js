6042f9749d464ebf25d88cdf973e1ff19a477506
sophia371A <141244136+sophia371A@users.noreply.github.com>
2024-10-30 12:08:19 -0300

    Add files via upload

---
+++ flashcardaula10-main/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Lepidoptera',
+    'Como as borboletas monarcas se defendem?',
+    'A Borboleta Monarca é uma borboleta venenosa, cujo as suas toxinas são adquiridas ainda quando é uma lagarta. '
+)
+
+criaCartao(
+    'Odonata',
+    'Há quantas espécies de libélulas pelo mundo?',
+    'No mundo, existem cerca de 3 mil espécies de libélulas.'
+)
+
+criaCartao(
+    'Mantodea',
+    'Louva-deuses são um dos insetos mais brutais, sabe por quẽ?',
+    'Os corpos dos Louva-deuses evoluiram para caçar o que os tornou predadores, podendo até mesmo aprender a pescar ou se alimentar de beija-flores.'
+) 
+
+criaCartao(
+    'Coleoptera',
+    'Do que os besouros Atlas se alimentam?',
+    'Os besouros Atlas preferem comida em decomposição, composto e madeira podre, mas também comem frutas estragadas e frescas. O besouro exibe um comportamento de necrófago, comendo o que a maioria das outras criaturas evitaria.'
+)
